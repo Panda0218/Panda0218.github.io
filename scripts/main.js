859849c5dc95c1ae2e34f80fdaf910d4b9f0841d
Panda0218 <crystal5771@163.com>
2018-07-24 20:58:05 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt("Enter your name");
+    console.log(myName);
+    localStorage.setItem("name", myName);
+myHeading.textContent="hello "+myName;
+}
+//初始化
+if(!localStorage.getItem("name")){
+	setUserName();
+}else{
+	var storedName=localStorage.getItem("name");
+	myHeading.textContent="hello"+storedName;
+}
+myButton.onclick = function() {
+    setUserName();
+}
